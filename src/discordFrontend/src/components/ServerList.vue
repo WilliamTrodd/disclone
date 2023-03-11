564da34da3d5b8eb1cc463badead605e63b0768d
WilliamTrodd <william.trodd@gmail.com>
2023-03-11 13:50:37 +0300

    tweaks to transitions

--- src/discordFrontend/src/components/ServerList.vue
+++ src/discordFrontend/src/components/ServerList.vue
@@ -12,18 +12,15 @@ const changeServer = (name: string, id: string) => {
 <template>
   <div class="flex flex-col bg-dc-grey-800 w-[72px] px-2 mx-auto pt-2 content-center">
     <div
-      class="h-12 w-12 rounded-full mb-2 overflow-hidden bg-dc-grey-text hover:bg-dc-blue-800 hover:transition-[border-radius] hover:duration-500 hover:rounded-[20px] hover:cursor-pointer">
+      class="h-12 w-12 rounded-[50%] mb-2 overflow-hidden bg-dc-grey-text hover:bg-dc-blue-800 transition-all duration-150 hover:rounded-[33%] hover:cursor-pointer">
     </div>
     <svg viewBox="0 0 100 20">
       <line x1="25" y1="0" x2="75" y2="0" class="stroke-dc-grey-text" />
     </svg>
 
-    <div v-for="server in props.servers" :key="server._id" class="inline-block h-12 w-12 rounded-[50%] mb-2 overflow-hidden bg-dc-blue-800 
-                              transition-all
-                              ease-in
-                              hover:rounded-[25%]
-                              duration-300
-                              " @click="changeServer(server.name, server._id)">
+    <div v-for="server in props.servers" :key="server._id"
+      class="inline-block h-12 w-12 rounded-[50%] mb-2 overflow-hidden bg-dc-blue-800 transition-all ease-in hover:rounded-[33%] duration-150"
+      @click="changeServer(server.name, server._id)">
       <img :src="server.icon" />
     </div>
   </div>
